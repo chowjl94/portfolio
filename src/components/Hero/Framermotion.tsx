660bef8dd204d776c9be36c269cc59a85fd91642
chowjl94 <chowjl94@gmail.com>
2024-03-22 14:47:41 +0800

    FIRST COMMIT

---
+++ src/components/Hero/Framermotion.tsx
@@ -0,0 +1,25 @@
+import { motion } from "framer-motion";
+
+const FramermotionScroll = () => {
+	return (
+		<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+			<a href="#about">
+				<div className="w-[35px] h-[64px] rounded-3xl border-4 border-[#f5f5f7] flex justify-center items-start p-2">
+					<motion.div
+						animate={{
+							y: [0, 20, 0],
+						}}
+						transition={{
+							duration: 1.5,
+							repeat: Infinity,
+							repeatType: "loop",
+						}}
+						className="w-3 h-3 rounded-full bg-[#f5f5f7] mb-1"
+					/>
+				</div>
+			</a>
+		</div>
+	);
+};
+
+export default FramermotionScroll;
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,34 @@
+import ComputersCanvas from "../canvas/Computers";
+import { styles } from "../../utils/styles";
+import FramermotionScroll from "./Framermotion";
+
+const Hero = () => {
+	return (
+		<section className={`relative w-full h-screen mx-auto`}>
+			<div
+				className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+			>
+				{/* <div className="flex flex-col justify-center items-center mt-5">
+					<div className={`w-5 h-5 rounded-full bg-[${fontColor}]`} />
+					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				</div> */}
+
+				<div>
+					<h1 className={`${styles.heroHeadText} text-white`}>
+						Hi, I'm <span className={`text-blue-500`}>Jing Lun</span>
+					</h1>
+					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
+						Web dev enthusiast <br className="sm:block hidden" />
+						with a passion for developing web applications
+					</p>
+				</div>
+			</div>
+
+			<ComputersCanvas />
+
+			<FramermotionScroll />
+		</section>
+	);
+};
+
+export default Hero;
